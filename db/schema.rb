b593c7ca0e615c69257486e4cfb047433532f101
dannyshafer <github@danielshafer.com>
2015-09-05 22:20:33 -0700

    do work

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902054350) do
+ActiveRecord::Schema.define(version: 20150831174537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "letters", force: :cascade do |t|
-    t.string   "template_name"
-    t.string   "your_name"
-    t.string   "position_name"
-    t.string   "company_name"
-    t.string   "hiring_manager_name"
-    t.text     "excitement"
-    t.text     "template"
-    t.integer  "user_id"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "conditions", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "countries", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "currencies", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "nifty_attachments", force: :cascade do |t|
@@ -280,6 +290,21 @@ ActiveRecord::Schema.define(version: 20150902054350) do
 
   add_index "shoppe_users", ["email_address"], name: "index_shoppe_users_on_email_address", using: :btree
 
+  create_table "stamps", force: :cascade do |t|
+    t.string   "subject"
+    t.string   "title"
+    t.string   "subtitle"
+    t.string   "image"
+    t.integer  "year"
+    t.integer  "country_id"
+    t.integer  "currency_id"
+    t.integer  "condition_id"
+    t.integer  "user_id"
+    t.float    "face_value"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -289,8 +314,8 @@ ActiveRecord::Schema.define(version: 20150902054350) do
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
